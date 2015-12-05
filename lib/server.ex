527ae5724d3d94fca32a674bfb89ce4cb6494345
Pat Wey <patrickwey@gmail.com>
2015-12-04 21:47:33 -0700

    Close #2: Accept connections on port 8080
    * Server.accept/1 takes a port and listens on it

--- lib/server.ex
+++ lib/server.ex
@@ -1,2 +1,16 @@
 defmodule Server do
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port,
+                                    [:binary,
+                                     packet: :line,
+                                     active: false,
+                                     reuseaddr: true])
+    if {:ok, client} = :gen_tcp.accept(socket) do
+      IO.puts "Got your request!"
+      :gen_tcp.close(socket)
+    else
+      IO.puts "Listening on port: #{port}"
+      accept(port)
+    end
+  end
 end
--- test/server_test.exs
+++ test/server_test.exs
@@ -1,7 +1,3 @@
 defmodule ServerTest do
   use ExUnit.Case
-
-  test "the truth" do
-    assert 1 + 1 == 3
-  end
 end

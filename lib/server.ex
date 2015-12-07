9e9b20312904489850bd5c59744031eab6687efb
Pat Wey <patrickwey@gmail.com>
2015-12-07 12:48:00 -0700

    Add test to parse headers, start implementing parse_headers/1

--- lib/server.ex
+++ lib/server.ex
@@ -26,11 +26,33 @@ defmodule Server do
   end
 
   def parse_request(request) do
-    # parse first line
-    [ first_line | headers ] = String.split(request, "\r\n")
+    [ head | body ] = String.split(request, "\r\n\r\n")
+    [ first_line | headers ] = String.split(head, "\r\n")
+
+    first_line = parse_first_line(first_line)
+    headers = parse_headers(headers)
+  end
+
+  def parse_first_line(first_line) do
     [ verb | path_and_protocol ] = String.split(first_line, " ")
     [ path | protocol ] = path_and_protocol
     [ protocol_version | _ ] = protocol
     [ verb: verb, path: path, protocol_version: protocol_version ]
+  end
+
+  def parse_headers(headers) do
+    headers = parse_headers(String.split(headers, "\r\n"), [])
+    IO.inspect(headers) # ??
+    parse_headers(headers, [])
+  end
+
+  def parse_headers([header | tail], parsed) do
+    [key, value] = String.split(header)
+    parsed = [{key, value} | parsed]
+    parse_headers(tail, parsed)
+  end
+
+  def parse_headers([], parsed) do
+    parsed
   end
 end
--- test/server_test.exs
+++ test/server_test.exs
@@ -1,7 +1,12 @@
 defmodule ServerTest do
   use ExUnit.Case
-  test "parse_request interprets and stores request" do
-    request = "GET / HTTP/1.1\r\nUser-Agent: curl/7.37.1\r\nHost: example.com\r\nAccept: /\r\n\r\n"
-    [ verb: "GET", path: "/", protocol_version: "HTTP/1.1" ] = Server.parse_request(request)
+  test "parse_first_line interprets and stores first line" do
+    first_line = "GET / HTTP/1.1"
+    [ verb: "GET", path: "/", protocol_version: "HTTP/1.1" ] = Server.parse_first_line(first_line)
+  end
+
+  test "parse_headers interprets and stores headers" do
+    headers = "User-Agent: curl/7.37.1\r\nHost: example.com\r\nAccept: /"
+    [] = Server.parse_headers(headers)
   end
 end

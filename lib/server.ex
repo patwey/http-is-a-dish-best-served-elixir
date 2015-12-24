3ed06035c776f8877e499b777394ba926ac7746d
Pat Wey <patrickwey@gmail.com>
2015-12-24 15:01:37 -0500

    Add comments

--- lib/server.ex
+++ lib/server.ex
@@ -1,4 +1,5 @@
 defmodule Server do
+  # Listen on specified port
   def accept(port) do
     {:ok, socket} = :gen_tcp.listen(port,
                                     [:binary,
@@ -8,6 +9,7 @@ defmodule Server do
     accept_loop(socket)
   end
 
+  # Accept requests on a port & serve them when they come in
   def accept_loop(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
 
@@ -21,6 +23,7 @@ defmodule Server do
     accept_loop(socket)
   end
 
+  # Separate out the request lines and parse them into a map
   def parse_request(request) do
     [request|body] = String.split(request, "\r\n\r\n")
     request_lines = String.split(request, "\r\n")
@@ -30,6 +33,7 @@ defmodule Server do
     |> parse_headers
   end
 
+  # Return a list containing the headers and a map of the parsed first line
   def parse_first_line(list, request \\ Map.new)
   def parse_first_line([first_line|headers], request) do
     [verb, path, protocol] = String.split(first_line, " ")
@@ -39,7 +43,7 @@ defmodule Server do
     [headers, request]
   end
 
-  # Handle requests with no headers
+  # Parse headers into k/v pairs, store in the request map
   def parse_headers([[""], request]), do: request
 
   def parse_headers([[header|tail], request]) do
@@ -50,12 +54,14 @@ defmodule Server do
 
   def parse_headers([[], request]), do: request
 
+  # Build a response given the request map
   def build_response(request) do
     request
     |> get_body
     |> respond_with
   end
 
+  # Use the requested path and a static root path to serve files
   @root_path Path.expand("~/code/server/files")
   def get_body(request) do
     file_path = Path.join(@root_path, request[:path])
@@ -65,12 +71,14 @@ defmodule Server do
     end
   end
 
+  # Create a response string with a dynamic content-length
   def respond_with(body) do
     "HTTP/1.0 200 OK \r\n" <>
     "Content-Length: #{String.length(body)}\r\n\r\n" <>
     body
   end
 
+  # Serve the client a response
   def serve(response, client) do
     :gen_tcp.send(client, response)
   end

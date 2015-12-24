5548221b44c1246c35f2bb871a0d7e49bf639702
Pat Wey <patrickwey@gmail.com>
2015-12-24 14:49:56 -0500

    Server retrieves file specified in path if it exists * Close #5 * Still needs to be tested

--- lib/server.ex
+++ lib/server.ex
@@ -12,28 +12,25 @@ defmodule Server do
     {:ok, client} = :gen_tcp.accept(socket)
 
     if {:ok, request} = :gen_tcp.recv(client, 0) do
-      parse_request(request)
-      serve(client)
+      request
+      |> parse_request
+      |> build_response
+      |> serve(client)
     end
 
     accept_loop(socket)
   end
 
-  def serve(client) do
-    response = 'HTTP/1.0 200 OK \r\n' ++
-               'Content-Length: 11\r\n\r\n' ++
-               'hello world'
-    :gen_tcp.send(client, response)
-  end
-
   def parse_request(request) do
     [request|body] = String.split(request, "\r\n\r\n")
-    request = String.split(request, "\r\n")
-    |> parse_first_line(Map.new)
+    request_lines = String.split(request, "\r\n")
+
+    request_lines
+    |> parse_first_line
     |> parse_headers
-    |> IO.inspect
   end
 
+  def parse_first_line(list, request \\ Map.new)
   def parse_first_line([first_line|headers], request) do
     [verb, path, protocol] = String.split(first_line, " ")
     request = Map.put(request, :verb, verb)
@@ -42,18 +39,39 @@ defmodule Server do
     [headers, request]
   end
 
-  # Handles requests with no headers
-  def parse_headers([[""], request]) do
-    request
-  end
+  # Handle requests with no headers
+  def parse_headers([[""], request]), do: request
 
-  def parse_headers([[header|headers], request]) do
+  def parse_headers([[header|tail], request]) do
     [k, v] = String.split(header, ": ")
     request = Map.put(request, k, v)
-    parse_headers([headers, request])
+    parse_headers([tail, request])
   end
 
-  def parse_headers([[], request]) do
+  def parse_headers([[], request]), do: request
+
+  def build_response(request) do
     request
+    |> get_body
+    |> respond_with
+  end
+
+  @root_path Path.expand("~/code/server/files")
+  def get_body(request) do
+    file_path = Path.join(@root_path, request[:path])
+    case File.read file_path do
+      {:ok, body}      -> body
+      {:error, reason} -> "Something went wrong: #{reason}"
+    end
+  end
+
+  def respond_with(body) do
+    "HTTP/1.0 200 OK \r\n" <>
+    "Content-Length: #{String.length(body)}\r\n\r\n" <>
+    body
+  end
+
+  def serve(response, client) do
+    :gen_tcp.send(client, response)
   end
 end
